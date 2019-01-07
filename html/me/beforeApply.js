42cff88c2fcf434b946ff4429d03bfee85c3fdce
wenxiao123 <821953863@qq.com>
2019-01-07 23:31:30 +0800

    暂无结果预报名

--- html/me/beforeApply.js
+++ html/me/beforeApply.js
@@ -11,57 +11,65 @@ define(function(require, exports, module) {
         },
         ready: function() {
             var _this = this;
-            var paging = {currentPage:1,showCount:5};
+            var paging = { currentPage: 1, showCount: 5 };
             layer.open({
                 content: "res.msg",
                 skin: 'msg',
                 time: 1
             })
-            
+
             Http.ajax({
                 url: "/student/queryStudentLectureSignList.do",
-                    async: false,
-                    data: {
-                        paging: paging
-                    },
-                    success: function(res){
-                        if(res.code == 200){
-                            if(res.data.paging.list) {
+                async: false,
+                data: {
+                    paging: paging
+                },
+                success: function(res) {
+                    if (res.code == 200) {
+                        if (res.data.paging) {
+                            if (res.data.paging.list) {
                                 console.log(res)
                                 _this.lectureList = res.data.paging.list;
                                 layer.open({
                                     content: "123",
                                     skin: 'msg',
                                     time: 1,
-                                    anim:false
+                                    anim: false
                                 })
-                            }else{
+                            } else {
                                 layer.open({
                                     content: "456",
                                     skin: 'msg',
                                     time: 1,
-                                    anim:false
+                                    anim: false
                                 })
                                 _this.showInfo = 3;
                             }
+                        } else {
+                            _this.showInfo = 3;
                         }
-                    },
-                    error: function(res){
-                        layer.open({
-                            content: res.msg,
-                            skin: 'msg',
-                            time: 1,
-                            anim:false
-                        })
-                        console.log(res)
+
+                    } else {
+                        _this.showInfo = 3;
                     }
+                },
+                error: function(res) {
+                    _this.showInfo = 3;
+                    layer.open({
+                        content: res.msg,
+                        skin: 'msg',
+                        time: 1,
+                        anim: false
+                    })
+                    console.log(res)
+                }
             })
         },
         methods: {
-            openLectureDetail(id){
+            openLectureDetail(id) {
                 api.openWin({
-                    url:"../lecture/detail_frame.html",
-                    pageParam:{
+                    url: "../lecture/detail_frame.html",
+                    pageParam: {
                         id: id
                     }
                 })
